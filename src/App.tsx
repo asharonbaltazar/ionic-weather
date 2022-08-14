2573c03b8eac3cb4963694a1b355d382600c8389
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-14 16:05:39 -0400

    [#5] - Remove useless css imports; import `setUpIonicReact` and call it

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "./store";
-import { Redirect, Route } from "react-router-dom";
-import { IonApp, IonRouterOutlet } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import Main from "./pages/Main";
-import SettingsModal from "./pages/Settings";
-import SearchModal from "./pages/Search";
-import ScreenLoader from "./components/ScreenLoader";
-import Week from "./pages/Week";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Main from './pages/Main';
+import SettingsModal from './pages/Settings';
+import SearchModal from './pages/Search';
+import ScreenLoader from './components/ScreenLoader';
+import Week from './pages/Week';
 // React Context
-import { AppProvider as ContextProvider } from "./context/app-context";
+import { AppProvider as ContextProvider } from './context/app-context';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Theme variables */
-import "./theme/variables.css";
+import './theme/variables.css';
 
+setupIonicReact();
 const App = () => {
   const { selectedWeather } = useSelector(
     (state: RootState) => state.weatherSlice
