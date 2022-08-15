0732a7c2bd267127aacff6883857e24ffc7a3326
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-15 14:16:38 -0400

    [#4] - Remove imports from `App.tsx`; create `index.css` and add to `index.html`

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,6 @@ import Week from './pages/Week';
 // React Context
 import { AppProvider as ContextProvider } from './context/app-context';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Theme variables */
-import './theme/variables.css';
-
 setupIonicReact();
 const App = () => {
   const { selectedWeather } = useSelector(
