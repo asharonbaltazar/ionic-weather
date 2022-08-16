f14c9f1f92ffc0d32a564c7c148c5c36fcd9482f
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-16 11:40:36 -0400

    [#4] - Replace `jsx` with `ts`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
