b38b1ca7dcc21fad25ab3b7be978e10fd83a4325
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-16 22:19:54 -0400

    [#11] - Remove `Header` props; move hook into `Header`

--- src/pages/Main/Header.tsx
+++ src/pages/Main/Header.tsx
@@ -2,38 +2,43 @@ import React from 'react';
 import { ActionIcon, Header as MantineHeader, Title } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
+import { useSelector } from 'react-redux';
+import { RootState } from '@store';
 
-interface HeaderProps {
-  address: string;
-  geolocation: boolean;
-}
+export const Header = () => {
+  const { selectedWeather } = useSelector(
+    (state: RootState) => state.weatherSlice
+  );
 
-export const Header = ({ address, geolocation }: HeaderProps) => (
-  <MantineHeader
-    className="flex items-center py-2 px-3 justify-between"
-    height="auto"
-  >
-    {address && (
-      <div className="flex items-center justify-center gap-x-2">
-        <Title className="text-gray-900 font-medium" order={5}>
-          {address}
-        </Title>
-        {geolocation && (
-          <Icon className="text-sm text-blue-600" icon="tabler:location" />
-        )}
+  const { address, geolocation } = selectedWeather;
+
+  return (
+    <MantineHeader
+      className="flex items-center py-2 px-3 justify-between"
+      height="auto"
+    >
+      {address && (
+        <div className="flex items-center justify-center gap-x-2">
+          <Title className="text-gray-900 font-medium" order={5}>
+            {address}
+          </Title>
+          {geolocation && (
+            <Icon className="text-sm text-blue-600" icon="tabler:location" />
+          )}
+        </div>
+      )}
+      <div className="flex items-center gap-x-3">
+        <Link to="/search">
+          <ActionIcon>
+            <Icon className="text-xl text-gray-900" icon="tabler:search" />
+          </ActionIcon>
+        </Link>
+        <Link to="/settings">
+          <ActionIcon>
+            <Icon className="text-xl text-gray-900" icon="tabler:settings" />
+          </ActionIcon>
+        </Link>
       </div>
-    )}
-    <div className="flex items-center gap-x-3">
-      <Link to="/search">
-        <ActionIcon>
-          <Icon className="text-xl text-gray-900" icon="tabler:search" />
-        </ActionIcon>
-      </Link>
-      <Link to="/settings">
-        <ActionIcon>
-          <Icon className="text-xl text-gray-900" icon="tabler:settings" />
-        </ActionIcon>
-      </Link>
-    </div>
-  </MantineHeader>
-);
+    </MantineHeader>
+  );
+};
