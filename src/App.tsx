daf7c5e0df97a482c8a9b415da267fb21dcf2ddf
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-16 22:23:33 -0400

    [#11] - `App.tsx` cleanup begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/store';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { Route, BrowserRouter } from 'react-router-dom';
 
-import ScreenLoader from '@components/ScreenLoader';
-import Main from '@pages/Main';
+import { MantineProvider } from '@mantine/core';
+
+import Main from '@pages/Main/Main';
 import SettingsModal from '@pages/Settings';
 import SearchModal from '@pages/Search';
-import Week from '@pages/Week';
 import { AppProvider as ContextProvider } from '@context/app-context';
 
-setupIonicReact();
-const App = () => {
-  const { selectedWeather } = useSelector(
-    (state: RootState) => state.weatherSlice
-  );
-
-  return (
+const App = () => (
+  <BrowserRouter>
     <ContextProvider>
-      <IonApp>
-        <IonReactRouter>
-          <IonRouterOutlet>
-            <Route exact path="/" component={Main} />
-            <Route path="/search" component={SearchModal} />
-            <Route path="/settings" component={SettingsModal} />
-            <Route path="/week">
-              {selectedWeather?.weather ? <Week /> : <Redirect to="/" />}
-            </Route>
-          </IonRouterOutlet>
-          <ScreenLoader />
-        </IonReactRouter>
-      </IonApp>
+      <MantineProvider withNormalizeCSS>
+        <Route path="/search" component={SearchModal} />
+        <Route path="/settings" component={SettingsModal} />
+        <Route path="/" exact component={Main} />
+      </MantineProvider>
     </ContextProvider>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
