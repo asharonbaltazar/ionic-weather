a08ddcce3cd718a95d4f3a0b86137901ec33e522
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-16 22:59:02 -0400

    #11 - Remove segment func

--- src/context/app-context.tsx
+++ src/context/app-context.tsx
@@ -4,17 +4,11 @@ import React, { createContext, ReactNode, useReducer } from 'react';
 interface IStoreState {
   alertModalState: boolean;
   alertButtonState: boolean;
-  segmentCarouselOption: 'today' | 'tomorrow';
   toggleAlertModal: (toggleOption?: boolean) => void;
   toggleAlertButton: () => void;
-  toggleSegmentsCarousel: (segmentItem: 'today' | 'tomorrow') => void;
 }
 interface IProps {
   children: ReactNode;
-}
-interface IToggleSegmentsCarousel {
-  type: 'TOGGLE_SEGMENTS_CAROUSEL';
-  payload: 'today' | 'tomorrow';
 }
 interface IToggleAlertModal {
   type: 'TOGGLE_ALERT_MODAL';
@@ -24,26 +18,19 @@ interface IToggleAlertBanner {
   type: 'TOGGLE_ALERT_BUTTON';
 }
 
-type Action = IToggleSegmentsCarousel | IToggleAlertModal | IToggleAlertBanner;
+type Action = IToggleAlertModal | IToggleAlertBanner;
 
 // Initial state
 const initialState: IStoreState = {
   alertModalState: false,
   alertButtonState: true,
-  segmentCarouselOption: 'today',
   toggleAlertModal: () => {},
   toggleAlertButton: () => {},
-  toggleSegmentsCarousel: () => {},
 };
 
 // Reducer
 const reducer = (state: IStoreState, action: Action): IStoreState => {
   switch (action.type) {
-    case 'TOGGLE_SEGMENTS_CAROUSEL':
-      return {
-        ...state,
-        segmentCarouselOption: action.payload,
-      };
     case 'TOGGLE_ALERT_MODAL':
       return {
         ...state,
@@ -66,10 +53,6 @@ const AppContext = createContext(initialState);
 const AppProvider = ({ children }: IProps) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const toggleSegmentsCarousel = (segmentItem: 'today' | 'tomorrow') => {
-    dispatch({ type: 'TOGGLE_SEGMENTS_CAROUSEL', payload: segmentItem });
-  };
-
   const toggleAlertModal = (toggleOption?: boolean) => {
     if (toggleOption !== undefined)
       dispatch({ type: 'TOGGLE_ALERT_MODAL', payload: toggleOption });
@@ -83,12 +66,10 @@ const AppProvider = ({ children }: IProps) => {
   return (
     <AppContext.Provider
       value={{
-        segmentCarouselOption: state.segmentCarouselOption,
         alertModalState: state.alertModalState,
         alertButtonState: state.alertButtonState,
         toggleAlertModal,
         toggleAlertButton,
-        toggleSegmentsCarousel,
       }}
     >
       {children}
