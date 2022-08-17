e94dcf0aa6e2773e4d6f782109207f3a7b653d22
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-16 22:24:00 -0400

    [#11] - `Main` clean up begin

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,49 +1,12 @@
 import React from 'react';
-import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/react';
 import { Header } from 'src/pages/Main/Header';
-import MainContent from '@components/MainContent';
-import MainPagePlaceholder from '@components/MainPagePlaceholder';
-import AlertModal from '@components/AlertModal';
-import { useSelector } from 'react-redux';
-import { useAppDispatch, RootState } from '@store';
-import { refreshWeatherData } from '@slices/weatherSlice';
-import '../css/main.css';
+import { Content } from 'src/pages/Main/Content';
 import { AppShell } from '@mantine/core';
 
-const Main = () => {
-  const dispatch = useAppDispatch();
-
-  const { selectedWeather } = useSelector(
-    (state: RootState) => state.weatherSlice
-  );
-
-  // refresher function
-  const refreshWeather = (e: CustomEvent) => {
-    dispatch(refreshWeatherData());
-    e.detail.complete();
-  };
-
-  return (
-    <AppShell>
-      <Header
-        address={selectedWeather.address}
-        geolocation={selectedWeather.geolocation}
-      />
-      <IonContent>
-        {selectedWeather?.weather ? (
-          <>
-            <IonRefresher slot="fixed" onIonRefresh={(e) => refreshWeather(e)}>
-              <IonRefresherContent />
-            </IonRefresher>
-            <MainContent />
-          </>
-        ) : (
-          <MainPagePlaceholder />
-        )}
-        <AlertModal />
-      </IonContent>
-    </AppShell>
-  );
-};
+const Main = () => (
+  <AppShell header={<Header />} padding={0}>
+    <Content />
+  </AppShell>
+);
 
 export default Main;
