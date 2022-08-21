60f5febc617069b8cfb439e1ed5ed7df40a44b93
asharonbaltazar <asharonbaltazar@outlook.com>
2022-08-21 15:15:44 -0400

    #11 - Create `GoogleAttribution` component

---
+++ src/pages/Main/SearchDrawer/GoogleAttribution.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import lightLogo from 'src/assets/google_light.png';
+import darkLogo from 'src/assets/google_dark.png';
+
+export const GoogleAttribution = () => (
+  <picture>
+    <source srcSet={darkLogo} media="(prefers-color-scheme: dark)" />
+    <img
+      className="h-6 w-36 object-contain"
+      src={lightLogo}
+      alt="google logo"
+    />
+  </picture>
+);
