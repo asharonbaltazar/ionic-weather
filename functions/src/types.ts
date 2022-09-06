7f9c818f2764e42ce451b4f97ca81653fb6a1814
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-06 14:28:13 -0400

    #13 - Create `OpenWeatherMapResponse` type

--- functions/src/types.ts
+++ functions/src/types.ts
@@ -17,3 +17,105 @@ export interface GMapGeocodeResult {
   lat: number;
   lng: number;
 }
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Minutely {
+  dt: number;
+  precipitation: number;
+}
+
+interface Hourly {
+  dt: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Weather[];
+  pop: string;
+}
+
+interface Daily {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Weather[];
+  clouds: number;
+  pop: number;
+  rain: number;
+  uvi: number;
+}
+
+interface Alert {
+  sender_name: string;
+  event: string;
+  start: number;
+  end: number;
+  description: string;
+  tags: string[];
+}
+
+export interface OpenWeatherMapResponse {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  current: {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: Weather[];
+  };
+  minutely: Minutely[];
+  hourly: Hourly[];
+  daily: Daily[];
+  alerts: Alert[];
+}
+
+export interface OWMWeather {}
