5112af087c7845b2e7986eff3a2bbdd017a0fd89
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-08 14:45:29 -0400

    #13 - Create `searchSlice.thunks` file

--- src/slices/searchSlice.thunks.ts
+++ src/slices/searchSlice.thunks.ts
@@ -7,15 +7,15 @@ export const getPredictionsByQuery = createAsyncThunk(
   'search/predictions',
   async (query: string, { rejectWithValue }) => {
     try {
-      const { data = [] } = await axios.get<
+      const { data: predictions = [] } = await axios.get<
         FunctionsResponse<GMapPrediction[]>
       >(`${import.meta.env.VITE_GET_GMAPS_SUGGESTIONS}?query=${query}`);
 
-      if (hasError(data)) {
-        return rejectWithValue(data.error);
+      if (hasError(predictions)) {
+        return rejectWithValue(predictions.error);
       }
 
-      return data;
+      return predictions;
     } catch (error) {
       console.error(error);
       return rejectWithValue('Woops! Something happened :(');
