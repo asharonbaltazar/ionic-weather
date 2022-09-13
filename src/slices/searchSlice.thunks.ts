74d77a93f46ae1d1c6e11a1803c7b79c1c765b2a
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-13 13:41:58 -0400

    #19 - Remove try/catch

--- src/slices/searchSlice.thunks.ts
+++ src/slices/searchSlice.thunks.ts
@@ -6,19 +6,14 @@ import axios from 'axios';
 export const getPredictionsByQuery = createAsyncThunk(
   'search/predictions',
   async (query: string, { rejectWithValue }) => {
-    try {
-      const { data: predictions = [] } = await axios.get<
-        FunctionsResponse<GMapPrediction[]>
-      >(`${import.meta.env.VITE_GET_GMAPS_SUGGESTIONS}?query=${query}`);
+    const { data: predictions = [] } = await axios.get<
+      FunctionsResponse<GMapPrediction[]>
+    >(`${import.meta.env.VITE_GET_GMAPS_SUGGESTIONS}?query=${query}`);
 
-      if (hasError(predictions)) {
-        return rejectWithValue(predictions.error);
-      }
-
-      return predictions;
-    } catch (error) {
-      console.error(error);
-      return rejectWithValue('Woops! Something happened :(');
+    if (hasError(predictions)) {
+      return rejectWithValue(predictions.error);
     }
+
+    return predictions;
   }
 );
