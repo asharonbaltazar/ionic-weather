a01409da194bb69e0a6c0a59e5d88202df518ac3
asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>
2022-09-19 14:53:22 -0400

    #26 - Sidebar (#28)
    * #26 - Create `Sidebar` component
    
    * #26 - Increase grid cols
    
    * #26 - Add `Sidebar` to `App`

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import { MediaQuery } from '@components/MediaQuery';
+import { LinkWithIcon } from '@components/LinkWithIcon';
+
+export const Sidebar = () => (
+  <MediaQuery>
+    <aside className="row-span-2 mr-4 flex flex-col items-center justify-between rounded-l-xl p-3 lg:bg-white lg:dark:bg-stone-900">
+      <div>
+        <LinkWithIcon to="/search" icon="tabler:search" />
+      </div>
+
+      <LinkWithIcon to="/settings" icon="tabler:settings" />
+    </aside>
+  </MediaQuery>
+);
