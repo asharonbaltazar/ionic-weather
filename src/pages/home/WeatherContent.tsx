51690d58441e9eee95b3bdd47c923bae6d2b9297
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-20 16:29:39 -0400

    #31 - Add loader to `CurrentForecast`

--- src/pages/home/WeatherContent.tsx
+++ src/pages/home/WeatherContent.tsx
@@ -2,25 +2,30 @@ import { MainPlaceholder } from '@pages/home/MainPagePlaceholder';
 import { CurrentForecast } from '@pages/home/CurrentForecast';
 import { LinkButtonWithIcon } from '@components/LInkButtonWithIcon';
 import { useWeather } from '@utilities/hooks';
+import { FullscreenLoading } from 'src/components/FullscreenLoading';
+import { Fragment } from 'react';
 
 export const WeatherContent = () => {
-  const { selectedWeather } = useWeather();
+  const { selectedWeather, loading } = useWeather();
 
   if (!selectedWeather) {
     return <MainPlaceholder />;
   }
 
   return (
-    <div className="mt-4 space-y-5 px-3">
-      <CurrentForecast selectedWeather={selectedWeather} />
-      <div className="space-y-2 lg:hidden">
-        <LinkButtonWithIcon icon="tabler:clock-hour-3" to="/hourly">
-          Hourly Forecast
-        </LinkButtonWithIcon>
-        <LinkButtonWithIcon icon="tabler:calendar-event" to="/weekly">
-          Weekly Forecast
-        </LinkButtonWithIcon>
+    <Fragment>
+      <FullscreenLoading loading={loading} />
+      <div className="mt-4 space-y-5 px-3">
+        <CurrentForecast selectedWeather={selectedWeather} />
+        <div className="space-y-2 lg:hidden">
+          <LinkButtonWithIcon icon="tabler:clock-hour-3" to="/hourly">
+            Hourly Forecast
+          </LinkButtonWithIcon>
+          <LinkButtonWithIcon icon="tabler:calendar-event" to="/weekly">
+            Weekly Forecast
+          </LinkButtonWithIcon>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
