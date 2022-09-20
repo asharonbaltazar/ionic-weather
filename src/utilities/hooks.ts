da47e6bb9a7767a9715ac079dba94742ad48023d
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-19 22:03:48 -0400

    #18 - Create `useFormatting` hook

--- src/utilities/hooks.ts
+++ src/utilities/hooks.ts
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@store';
+import { formatSpeed, formatTemp } from '@utilities/format';
+import dayjs from 'dayjs';
 
 export const useWeather = () =>
   useSelector((state: RootState) => state.weatherSlice);
@@ -9,3 +11,15 @@ export const useSettings = () =>
 
 export const useSearch = () =>
   useSelector((state: RootState) => state.searchSlice);
+
+export const useFormatting = () => {
+  const { temperature, windSpeed, time } = useSettings();
+
+  return {
+    formatTemp: (temp: number) =>
+      `${formatTemp[temperature](temp)}°${temperature.toUpperCase()}`,
+    formatWindSpeed: (speed: number) =>
+      `${formatSpeed[windSpeed](speed)} ${windSpeed}`,
+    formatTime: (dateAndTime: string) => dayjs(dateAndTime).format(time),
+  };
+};
