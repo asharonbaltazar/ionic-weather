afc6dc47c0f2e69b4a81148c2bdf689b133af254
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-19 21:18:36 -0400

    #18 - Remove border radius and margin

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { LinkWithIcon } from '@components/LinkWithIcon';
 
 export const Sidebar = () => (
   <MediaQuery>
-    <aside className="row-span-2 mr-4 flex flex-col items-center justify-between rounded-l-xl p-3 lg:bg-white lg:dark:bg-stone-900">
+    <aside className="row-span-2 flex flex-col items-center justify-between rounded-l-xl p-3 lg:bg-white lg:dark:bg-stone-900">
       <div>
         <LinkWithIcon to="/search" icon="tabler:search" />
       </div>
