08138090b35e3c28b38df30a4722a71339249428
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-19 22:04:57 -0400

    #18 - Format placeholder for new grid styles

--- src/pages/Main/MainPagePlaceholder.tsx
+++ src/pages/Main/MainPagePlaceholder.tsx
@@ -1,14 +1,13 @@
 import { Icon } from '@iconify/react';
 
 export const MainPlaceholder = () => (
-  <div className="flex h-full flex-col items-center pt-32">
+  <div className="flex h-full flex-col items-center pt-32 lg:col-span-3">
     <Icon className="text-7xl text-blue-500" icon="tabler:rainbow" />
     <h1 className="text mt-5 text-3xl font-medium">Welcome to Ionic Weather</h1>
     <div className="mt-3">
       <h3 className="flex items-center text-xl dark:text-stone-200/50">
         To search, tap the
         <Icon className="mx-1 text-blue-500" icon="tabler:search" />
-        above
       </h3>
     </div>
   </div>
