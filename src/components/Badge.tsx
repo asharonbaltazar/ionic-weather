151a6353c4774cc39fb79af04cede41cd20a45c2
asharonbaltazar <asharonbaltazar@outlook.com>
2022-09-29 15:26:32 -0400

    #21 - Badge now takes `bgColor` and `textColor`

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,11 +1,24 @@
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 interface BadgeProps {
+  bgColor?: string;
+  textColor?: string;
   children: ReactNode;
 }
 
-export const Badge = ({ children }: BadgeProps) => (
-  <span className="inline-flex items-center rounded-lg bg-blue-200 px-2 py-1.5 text-xs font-medium text-blue-800">
+export const Badge = ({
+  bgColor = 'bg-blue-200',
+  textColor = 'text-gray-100',
+  children,
+}: BadgeProps) => (
+  <span
+    className={clsx(
+      'inline-flex items-center whitespace-nowrap rounded-lg px-2 py-1.5 text-xs font-medium',
+      bgColor,
+      textColor
+    )}
+  >
     {children}
   </span>
 );
