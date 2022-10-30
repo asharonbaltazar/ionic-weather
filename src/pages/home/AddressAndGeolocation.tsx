5c3effe8b70de333157953c52917e1f5862b24e5
asharonbaltazar <asharonbaltazar@outlook.com>
2022-10-30 15:51:08 -0400

    #45 - Styling fixes

--- src/pages/home/AddressAndGeolocation.tsx
+++ src/pages/home/AddressAndGeolocation.tsx
@@ -14,8 +14,8 @@ export const AddressAndGeolocation = ({
   }
 
   return (
-    <div className="flex grow items-center justify-center gap-x-2 lg:col-start-2 lg:justify-between lg:bg-white lg:p-3 lg:shadow-sm lg:dark:bg-stone-900 lg:dark:shadow-none">
-      <span className="grow">
+    <div className="flex grow items-center lg:col-start-2 lg:bg-white lg:p-3 lg:shadow-sm lg:dark:bg-stone-900 lg:dark:shadow-none">
+      <span className="flex grow items-center justify-center gap-x-2 pl-5 lg:justify-start lg:pl-0">
         <h5 className="text text-xl font-medium">{selectedWeather.address}</h5>
         {selectedWeather.isGeolocation && (
           <Icon className="text-sm text-blue-600" icon="tabler:location" />
--- src/pages/home/ReloadWeatherButton.tsx
+++ src/pages/home/ReloadWeatherButton.tsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 export const ReloadWeatherButton = () => (
   <button className="text group items-center gap-x-2 rounded font-medium dark:hover:bg-slate-700">
     <Icon
-      className="m-1 text-2xl text-blue-500 group-hover:rotate-[360deg] group-hover:transition group-hover:duration-300 group-hover:ease-in-out dark:text-blue-300 md:text-3xl"
+      className="m-1 text-2xl text-blue-500 group-hover:rotate-[360deg] group-hover:transition group-hover:duration-300 group-hover:ease-in-out dark:text-slate-400 md:text-3xl"
       icon="tabler:reload"
     />
   </button>
