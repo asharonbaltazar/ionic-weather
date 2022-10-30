b7b20371c2585585bb5925d4b791723960b939e0
asharonbaltazar <asharonbaltazar@outlook.com>
2022-10-30 12:45:56 -0400

    #45 - Create ReloadWeatherButton component

---
+++ src/pages/home/ReloadWeatherButton.tsx
@@ -0,0 +1,10 @@
+import { Icon } from '@iconify/react';
+
+export const ReloadWeatherButton = () => (
+  <button className="text group items-center gap-x-2 rounded font-medium dark:hover:bg-slate-700">
+    <Icon
+      className="m-1 text-2xl text-blue-500 group-hover:rotate-[360deg] group-hover:transition group-hover:duration-300 group-hover:ease-in-out dark:text-blue-300 md:text-3xl"
+      icon="tabler:reload"
+    />
+  </button>
+);
