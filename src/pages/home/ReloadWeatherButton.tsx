f16f3233e1c0a68f9cf0d1fef1671d2de0af313e
asharonbaltazar <asharonbaltazar@outlook.com>
2022-10-30 21:37:50 -0400

    #45 - Add `selectedWeather` props

--- src/pages/home/ReloadWeatherButton.tsx
+++ src/pages/home/ReloadWeatherButton.tsx
@@ -1,10 +1,24 @@
+import { Weather } from '@functions/types';
 import { Icon } from '@iconify/react';
 
-export const ReloadWeatherButton = () => (
-  <button className="text group items-center gap-x-2 rounded font-medium dark:hover:bg-slate-700">
-    <Icon
-      className="m-1 text-2xl text-blue-500 group-hover:rotate-[360deg] group-hover:transition group-hover:duration-300 group-hover:ease-in-out dark:text-slate-400 md:text-3xl"
-      icon="tabler:reload"
-    />
-  </button>
-);
+interface ReloadWeatherButtonProps {
+  selectedWeather: Weather;
+}
+
+export const ReloadWeatherButton = ({
+  selectedWeather,
+}: ReloadWeatherButtonProps) => {
+  const onReload = () => {};
+
+  return (
+    <button
+      className="text group items-center gap-x-2 rounded font-medium dark:hover:bg-slate-700"
+      onClick={onReload}
+    >
+      <Icon
+        className="m-1 text-2xl text-blue-500 group-hover:rotate-[360deg] group-hover:transition group-hover:duration-300 group-hover:ease-in-out dark:text-slate-400 md:text-3xl"
+        icon="tabler:reload"
+      />
+    </button>
+  );
+};
