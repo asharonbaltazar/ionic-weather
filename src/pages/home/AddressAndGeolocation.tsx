e7a1af0512f6331e959145eaeeef0d4c0ec43ed7
asharonbaltazar <asharonbaltazar@outlook.com>
2022-10-30 21:37:10 -0400

    #45 - Styling changes

--- src/pages/home/AddressAndGeolocation.tsx
+++ src/pages/home/AddressAndGeolocation.tsx
@@ -1,6 +1,6 @@
 import { StateWeather } from '@functions/types';
 import { Icon } from '@iconify/react';
-import { ReloadWeatherButton } from './ReloadWeatherButton';
+import { ReloadWeatherButton } from '@pages/home/ReloadWeatherButton';
 
 interface AddressAndGeolocationProps {
   selectedWeather: StateWeather | null;
@@ -14,14 +14,16 @@ export const AddressAndGeolocation = ({
   }
 
   return (
-    <div className="flex grow items-center lg:col-start-2 lg:bg-white lg:p-3 lg:shadow-sm lg:dark:bg-stone-900 lg:dark:shadow-none">
-      <span className="flex grow items-center justify-center gap-x-2 pl-5 lg:justify-start lg:pl-0">
-        <h5 className="text text-xl font-medium">{selectedWeather.address}</h5>
+    <div className="flex grow items-center truncate lg:col-start-2 lg:bg-white lg:p-3 lg:shadow-sm lg:dark:bg-stone-900 lg:dark:shadow-none">
+      <span className="flex grow items-center justify-center gap-x-2 truncate pl-5 lg:justify-start lg:pl-0">
+        <h5 className="text truncate text-xl font-medium">
+          {selectedWeather.address}
+        </h5>
         {selectedWeather.isGeolocation && (
           <Icon className="text-sm text-blue-600" icon="tabler:location" />
         )}
       </span>
-      <ReloadWeatherButton />
+      <ReloadWeatherButton selectedWeather={selectedWeather} />
     </div>
   );
 };
