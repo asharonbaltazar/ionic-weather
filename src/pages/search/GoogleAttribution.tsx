c8019eab655513332fd06f6d4ca03fa07b9bf1b9
asharonbaltazar <asharonbaltazar@outlook.com>
2023-07-25 10:22:12 -0400

    move assets to `src`

--- src/pages/search/GoogleAttribution.tsx
+++ src/pages/search/GoogleAttribution.tsx
@@ -1,12 +1,13 @@
-import lightLogo from '@assets/google_light.png';
-import darkLogo from '@assets/google_dark.png';
-
 export const GoogleAttribution = () => (
   <picture className="mt-3 self-end pb-3 pr-3">
-    <img className="h-6 w-36 object-contain" src={darkLogo} alt="google logo" />
+    <img
+      className="h-6 w-36 object-contain"
+      src="assets/google_dark.png"
+      alt="google logo"
+    />
     <img
       className="h-6 w-36 object-contain dark:hidden"
-      src={lightLogo}
+      src="assets/google_light.png"
       alt="google logo"
     />
   </picture>
