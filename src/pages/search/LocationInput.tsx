481b6a66312079bc94875c2253c03ef947fac4fc
asharonbaltazar <asharonbaltazar@outlook.com>
2023-07-25 10:14:32 -0400

    simplify `LocationInput` logic

--- src/pages/search/LocationInput.tsx
+++ src/pages/search/LocationInput.tsx
@@ -1,40 +1,27 @@
 import { useState } from 'react';
-import { resetLocations, setSearchLoading } from '@slices/searchSlice';
-import { getLocationByQuery } from '@slices/searchSlice.thunks';
 import { useDebouncedValue, useDidUpdate } from '@mantine/hooks';
 import { Input } from '@components/Input';
-import { useSearch } from '@utilities/hooks';
-import { useAppDispatch } from '@store';
 
-export const LocationInput = () => {
+type LocationInputProps = {
+  getLocation: (locationString: string) => void;
+};
+
+export const LocationInput = ({ getLocation }: LocationInputProps) => {
   const [locationQuery, setLocationQuery] = useState('');
   const [debouncedLocationQuery] = useDebouncedValue(locationQuery, 750);
 
-  const dispatch = useAppDispatch();
-  const { loading } = useSearch();
-
   useDidUpdate(() => {
     if (debouncedLocationQuery.trim().length) {
-      dispatch(getLocationByQuery(debouncedLocationQuery));
-    } else {
-      dispatch(resetLocations());
+      getLocation(debouncedLocationQuery);
     }
   }, [debouncedLocationQuery]);
-
-  const onSearch = (newQuery: string) => {
-    if (!loading && newQuery.trim().length) {
-      return dispatch(setSearchLoading(true));
-    }
-
-    return setLocationQuery(newQuery);
-  };
 
   return (
     <div className="mt-4 px-3">
       <Input
         placeholder="e.g. Baltimore"
         label="City Name"
-        onChange={(e) => onSearch(e.currentTarget.value)}
+        onChange={(e) => setLocationQuery(e.currentTarget.value)}
         value={locationQuery}
       />
     </div>
